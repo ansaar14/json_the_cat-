f901fb2c2f1db042568f4c4b8d2c9ca97daf8c73
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-04 20:25:38 +0000

    refractored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,30 @@
 const request = require('request');
-const breedName = process.argv[2];
 
+const fetchBreedDescription = function(breedName, callback) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    return console.log("error", error);
-  }
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(`error: ${error}`, null);
+    
+    }
 
-  // const firstEntry = data[0].description
-  // console.log(firstEntry);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log(`${breedName} not found!`);
-  }
+    const data = JSON.parse(body);
+    // console.log(data);
+    // console.log(typeof data);
+
+    // const firstEntry = data[0].description
+    // console.log(firstEntry);
+    if (data[0]) {
+      callback(null, data[0].description);
+    
+    } else {
+      callback(`${breedName} not found!`, null);
+    
+    }
   
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
